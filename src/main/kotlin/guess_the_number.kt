b8b286e5bcc67ee7e1f16ad4bf01bdf1caf27847
PATYA <patysik@mail.ru>
2023-12-24 20:14:01 +0300

    продолжить игру?

--- src/main/kotlin/guess_the_number.kt
+++ src/main/kotlin/guess_the_number.kt
@@ -1,17 +1,22 @@
 import java.util.*
 import kotlin.random.Random
 
-fun main(args: Array<String>)
-{
-    val randomNumber = Random.nextInt(1,11)
+fun main(args: Array<String>) {
+    do {
+        val randomNumber = Random.nextInt(1, 11)
 
-    println("Select any number from 1 until 10")
-    val playerNumber = Scanner(System.`in`).nextInt()
+        println("Select any number from 1 until 10")
+        val playerNumber = Scanner(System.`in`).nextInt()
 
-    when( randomNumber)
-    {  playerNumber -> println("Yes! You are winner!")
-       else -> println("No! You are loser!")
-    }
-    println()
+        when (randomNumber) {
+            playerNumber -> println("Yes! You are winner!")
+            else -> println("No! You are loser!")
+        }
+        println()
+        println("Do you want to continue? y/n")
+        val agree = Scanner(System.`in`).next().toLowerCase() == "yes"
+        println()
+    } while (agree)
+
     println("Good bye!See you later!")
 }
