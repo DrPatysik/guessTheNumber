742e579a6e9be9d16d8ff48791c5164376f67e0a
PATYA <patysik@mail.ru>
2023-12-25 01:17:44 +0300

    добавила 3 попытки

--- src/main/kotlin/guess_the_number.kt
+++ src/main/kotlin/guess_the_number.kt
@@ -2,21 +2,32 @@ import java.util.*
 import kotlin.random.Random
 
 fun main(args: Array<String>) {
+
+    var counter:Int = 3
     do {
+        println("You have $counter attempts! ")
         val randomNumber = Random.nextInt(1, 11)
 
-        println("Select any number from 1 until 10")
+        println(" Print any number from 1 until 10. ")
         val playerNumber = Scanner(System.`in`).nextInt()
 
-        when (randomNumber) {
-            playerNumber -> println("Yes! You are winner!")
-            else -> println("No! You are loser!")
+        when (playerNumber) {
+            randomNumber -> {println("\n Yes! Congratulation! You are winner!!!!")
+            break }
+            in 1..randomNumber-> println("\n No! Your number is less ! ")
+            in randomNumber..10-> println("\n No! Your number is greater! ")
+            else -> println("\n Your range is wrong!!")
         }
-        println()
-        println("Do you want to continue? y/n")
-        val agree = Scanner(System.`in`).next().toLowerCase() == "yes"
-        println()
-    } while (agree)
+        when(counter){
+            in 2..3 -> println("Do you want to continue? yes/no")
+             1 -> break
+        }
+        val agree = Scanner(System.`in`).next().toLowerCase()
+        counter--
+        if(agree != "no" && agree !="yes")
+        { println("Your answer is wrong!")}
 
-    println("Good bye!See you later!")
+    } while (agree == "yes" && counter !=0 )
+
+    println("Good bye!See you later! Come back again!!")
 }
